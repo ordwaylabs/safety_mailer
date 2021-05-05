767f3041fb6c5c25d2b11881c1aa4ed0593af974
kunalvashistordway <50451412+kunalvashistordway@users.noreply.github.com>
2021-05-05 13:02:03 +0530

    Remove disallowed recipients from CC/BCC fields
    Remove disallowed recipients from CC/BCC fields too. Make Carrier#deliver! behave like original #deliver!: raise an exception when there're no recipients initially and return mail object.

--- lib/safety_mailer/safety_mailer.rb
+++ lib/safety_mailer/safety_mailer.rb
@@ -1,90 +1,52 @@
 module SafetyMailer
   class Carrier
-    attr_accessor :matchers, :settings, :mail
+    attr_accessor :allowed_matchers, :settings
 
     def initialize(params = {})
-      self.matchers = params[:allowed_matchers] || []
+      self.allowed_matchers = params[:allowed_matchers] || []
       self.settings = params[:delivery_method_settings] || {}
       delivery_method = params[:delivery_method] || :smtp
-      @delivery_method = ActionMailer::Base.delivery_methods[delivery_method].new(settings)
-      @sendgrid_options = {}
+      @delivery_method = Mail::Configuration.instance.lookup_delivery_method(delivery_method).new(settings)
     end
 
     def deliver!(mail)
-      self.mail = mail
-      allowed = filter(recipients)
-
-      if allowed.empty?
-        log "*** safety_mailer - no allowed recipients ... suppressing delivery altogether"
-        return
+      original_recipients = Array(mail.to).
+        concat(Array(mail.cc)).
+        concat(Array(mail.bcc))
+      if original_recipients.empty?
+        raise ArgumentError, "At least one recipient (To, Cc or Bcc) is required to send a message"
       end
 
-      mail['X-SMTPAPI'].value = prepare_sendgrid_delivery(allowed) if sendgrid?
-      mail.to = allowed
+      remove_disallowed_recipients!(mail)
 
-      @delivery_method.deliver!(mail)
-    end
+      allowed_recipients = mail.to.dup.
+        concat(mail.cc).
+        concat(mail.bcc)
 
-    def whitelisted?(recipient)
-      matchers.any? { |m| recipient =~ m }
-    end
-
-    def filter(addresses)
-      allowed, rejected = addresses.partition { |r| whitelisted?(r) }
-
-      rejected.each { |addr| log "*** safety_mailer delivery suppressed for #{addr}" }
-      allowed.each { |addr| log "*** safety_mailer delivery allowed for #{addr}" }
-
-      allowed
-    end
-
-  private
-
-    def recipients
-      sendgrid?
-      sendgrid_to = @sendgrid_options['to']
-      sendgrid_to.nil? || sendgrid_to.empty? ? mail.to : sendgrid_to || []
-    end
-
-    def sendgrid?
-      @sendgrid ||= !!if mail['X-SMTPAPI']
-        @sendgrid_options = JSON.parse(mail['X-SMTPAPI'].value)
+      if allowed_recipients.empty?
+        log "safety_mailer: no allowed recipients. Suppressing delivery"
+        mail
+      else
+        log "safety_mailer: allowing delivery to #{allowed_recipients}"
+        log "safety_mailer: original email below:"
+        @delivery_method.deliver!(mail)
       end
-    rescue JSON::ParserError
-      log "*** safety_mailer was unable to parse the X-SMTPAPI header"
     end
 
-    # Handles clean-up for additional SendGrid features that may be required
-    # by changes to the recipient list. Expects the passed-in Array of
-    # addresses to have been whitelist-filtered already.
-    def prepare_sendgrid_delivery(addresses)
-      amendments = { 'to' => addresses }
+    private
 
-      # The SendGrid Substitution Tags feature, if used, requires that an
-      # ordered Array of substitution values aligns with the Array of
-      # recipients in the "to" field of the API header. If substitution key is
-      # present, this filters the Arrays for each template to re-align with our
-      # whitelisted addresses.
-      #
-      # @see http://docs.sendgrid.com/documentation/api/smtp-api/developers-guide/substitution-tags/
-      if substitutions = @sendgrid_options['sub']
-        substitutions.each do |template, values|
-          values = recipients.zip(values).map do |addr, value|
-            value if addresses.include?(addr)
-          end
+    def remove_disallowed_recipients!(mail)
+      recipient_allowed = lambda { |recipient|
+        allowed_matchers.any? { |m| recipient =~ m }
+      }
 
-          substitutions[template] = values.compact
-        end
-
-        amendments['sub'] = substitutions
-      end
-
-      JSON.generate(@sendgrid_options.merge(amendments))
+      mail.to = Array(mail.to).select(&recipient_allowed)
+      mail.cc = Array(mail.cc).select(&recipient_allowed)
+      mail.bcc = Array(mail.bcc).select(&recipient_allowed)
     end
 
     def log(msg)
       Rails.logger.warn(msg) if defined?(Rails)
     end
-
   end
 end
